a19a43e96cbde0ae2bdbb7d9da4d5776d9b75906
22055204@student.westernsydney.edu.au <22055204@student.westernsydney.edu.au>
2026-03-24 20:39:45 +1100

    Added function logic to history and tags.ts files

--- apps/web/src/functions/history.ts
+++ apps/web/src/functions/history.ts
@@ -3,5 +3,11 @@ export function history(posts: { date: Date; active: boolean }[]): string[] {
   // Return the ordered list of "month, year" strings sorted from most recent to oldes
   // consider only active posts
 
+  /* Function logic
+  1. Filter active posts
+  2. Map the dates into a string format like "January 2026"
+  3. Uniqueify: Use a set or reduce to ensure January 2026 only appears once even if there 10 posts in that month
+  4. Sort: Ensure the most recent dates are at the top
+  */
   return [];
 }
--- apps/web/src/functions/tags.ts
+++ apps/web/src/functions/tags.ts
@@ -2,5 +2,12 @@
 
 export async function tags(posts: { tags: string; active: boolean }[]) {
   // TODO: Implement per specification
+
+  /* Function logic
+    1. Filter active posts
+    2. Split and Flatten: if a post has "react, CSS" you need to split it by the comma and flatten it so you just have a big list of individual words
+    3. Count: just like categories, you'll likely want to count how many times each tag appears
+  */
+
   return [];
 }
