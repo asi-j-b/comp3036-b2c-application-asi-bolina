e16201f5fe674c1a44c51f934ed72447c694df91
22055204@student.westernsydney.edu.au <22055204@student.westernsydney.edu.au>
2026-03-25 17:48:59 +1100

    Using map to look through posts array and build a unique list item component

--- apps/web/src/components/Blog/List.tsx
+++ apps/web/src/components/Blog/List.tsx
@@ -1,7 +1,14 @@
 import type { Post } from "@repo/db/data";
+import { BlogListItem } from "./ListItem"; // Importing the item component
 
 export function BlogList({ posts }: { posts: Post[] }) {
-  return <div className="py-6">List (source file: List.tsx)</div>;
+  return (
+    <div className="py-6">
+      {posts.map((post) => (
+        <BlogListItem key = {post.id} post={post} />
+      ))}
+      </div>
+  );
 }
 
 export default BlogList;
