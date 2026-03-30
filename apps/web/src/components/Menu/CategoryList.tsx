d747eef612cb7279681bbf71b64f09a9f7c750eb
22055204@student.westernsydney.edu.au <22055204@student.westernsydney.edu.au>
2026-03-30 16:08:45 +1100

    UseState and Effect for async categories logic

--- apps/web/src/components/Menu/CategoryList.tsx
+++ apps/web/src/components/Menu/CategoryList.tsx
@@ -1,15 +1,23 @@
+"use client";
+
 import { categories } from "@/functions/categories";
 import type { Post } from "@repo/db/data";
 import { toUrlPath } from "@repo/utils/url";
 import { SummaryItem } from "./SummaryItem";
+import { useState, useEffect } from "react";
 
-export function CategoryList({
-  posts,
-  selectedCategory,
-}: {
-  posts: Post[];
-  selectedCategory?: string;
-}) {
+export function CategoryList({ posts }: { posts: Post[] }) {
+
+  const [data, setData] = useState<{ name: string; count: number}[]>([]);
+  
+  useEffect(() => {
+    const loadCategories = async () => {
+      const result = await categories(posts);
+      setData(result);
+    };
+    loadCategories();
+  }, [posts]);
+
   // TODO: Implement proper category list
   return (
     <>
@@ -18,7 +26,7 @@ export function CategoryList({
           key={item.name}
           count={item.count}
           name={item.name}
-          isSelected={toUrlPath(item.name) === selectedCategory?.toLowerCase()}
+          isSelected={false}
           link={`/category/${toUrlPath(item.name)}`}
           title=""
         />
