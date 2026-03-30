465d7c7ced6f7b0b9f8828294bfb8dfc9e687180
22055204@student.westernsydney.edu.au <22055204@student.westernsydney.edu.au>
2026-03-30 19:44:11 +1100

    Wrapped heading in a Link component

--- apps/web/src/components/Blog/Detail.tsx
+++ apps/web/src/components/Blog/Detail.tsx
@@ -1,12 +1,15 @@
 import type { Post } from "@repo/db/data";
 import { marked } from "marked";
+import Link from "next/link";
 
 export async function BlogDetail({ post }: { post: Post }) {
   const content = await marked.parse(post.content);
 
   return (
     <article data-test-id={`blog-post-${post.id}`}>
-      <h1>{post.title}</h1>
+      <Link href={`/post/${post.urlId}`}>
+        <h1>{post.title}</h1>
+      </Link>
       <p>{post.category.charAt(0).toUpperCase() + post.category.slice(1)}</p>
 
       <p>{post.views} views</p> 
