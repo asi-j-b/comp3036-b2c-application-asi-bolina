090f99a244b3402628a86063ceb203ab5bc7104f
22055204@student.westernsydney.edu.au <22055204@student.westernsydney.edu.au>
2026-03-30 16:34:37 +1100

    All component functions are now async

--- apps/web/src/components/Menu/TagList.tsx
+++ apps/web/src/components/Menu/TagList.tsx
@@ -1,31 +1,32 @@
+"use client";
+
 import { type Post } from "@repo/db/data";
-import { tags as tag } from "@/functions/tags";
+import { tags } from "../../functions/tags";
 import { LinkList } from "./LinkList";
 import { SummaryItem } from "./SummaryItem";
 import { toUrlPath } from "@repo/utils/url";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
-export async function TagList({
+export function TagList({
   selectedTag,
   posts,
 }: {
   selectedTag?: string;
   posts: Post[];
 }) {
-
-  const [data, setData] = useState<{ name: string; count: number}[]>([]);
+  const [data, setData] = useState<{ name: string; count: number }[]>([]);
 
   useEffect(() => {
     const loadTags = async () => {
-      const postTags = await tag(posts);
-      setData(postTags);
+      const result = await tags(posts);
+      setData(result);
     };
+    
     loadTags();
   }, [posts]);
 
   return (
     <LinkList title="Tags">
-      Tags {/* Todo implement, use the summary item */}
       {data.map((tag) => (
         <SummaryItem
           key={tag.name}
@@ -34,7 +35,7 @@ export async function TagList({
           isSelected={toUrlPath(tag.name) === selectedTag?.toLowerCase()}
           link={`/tag/${toUrlPath(tag.name)}`}
           title={tag.name}
-        />  
+        />
       ))}
     </LinkList>
   );
