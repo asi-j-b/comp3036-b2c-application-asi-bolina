9df4ca7b2f5ceea1e9de3d7c53825afd9dc8c9e5
22055204@student.westernsydney.edu.au <22055204@student.westernsydney.edu.au>
2026-04-07 16:54:13 +1000

    Adding code

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { prisma } from "@repo/db";
 import { posts } from "@repo/db/data";
 import { isLoggedIn } from "../utils/auth";
 import styles from "./page.module.css";
@@ -6,6 +7,8 @@ export default async function Home() {
   // use the is logged in function to check if user is authorised
   // we will use the cookie based approach
   const loggedIn = await isLoggedIn();
+
+  // new
 
   if (!loggedIn) {
     return <main>Not logged in</main>;
