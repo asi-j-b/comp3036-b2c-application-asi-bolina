d6c2c902f9cbea51d6d19e21210fdc810134d60a
22055204@student.westernsydney.edu.au <22055204@student.westernsydney.edu.au>
2026-04-07 20:53:03 +1000

    Created client component and imported on home page

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -2,23 +2,27 @@ import { prisma } from "@repo/db";
 import { redirect } from "next/navigation";
 import { isLoggedIn } from "../utils/auth";
 import styles from "./page.module.css";
+import { AdminFilters } from "../components/AdminFilters";
 
-export default async function Home() {
-  // use the is logged in function to check if user is authorised
-  // we will use the cookie based approach
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { query?: string; tag?: string; sort?: string };
+} ) {
+  
   const loggedIn = await isLoggedIn();
 
   if (!loggedIn) {
     return <main>Not logged in</main>;
-  } else {
-    return (
-      <main className={styles.main}>
-        <ul>
-          {posts.map((p) => (
-            <li key={p.id}>{p.title}</li>
-          ))}
-        </ul>
-      </main>
-    );
   }
+  
+  return (
+    <main className={styles.main}>
+      <ul>
+        {posts.map((p) => (
+          <li key={p.id}>{p.title}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }
