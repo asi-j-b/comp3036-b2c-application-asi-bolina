1e9f81c3424050b1a9478161ae30e57345b08466
22055204@student.westernsydney.edu.au <22055204@student.westernsydney.edu.au>
2026-05-25 13:07:12 +1000

    Responsive side menu when window resized

--- apps/web/src/components/Layout/AppLayoutClient.tsx
+++ apps/web/src/components/Layout/AppLayoutClient.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, type PropsWithChildren } from "react";
+import { useEffect, useState, type PropsWithChildren } from "react";
 import { LeftMenu } from "../Menu/LeftMenu";
 import { Content } from "../Content";
 import { SidebarContext } from "@/context/SidebarContext";
@@ -11,9 +11,31 @@ export function AppLayoutClient({
 }: PropsWithChildren) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(min-width: 1024px)");
+
+    const syncSidebar = (event?: MediaQueryListEvent) => {
+      if ((event?.matches ?? mediaQuery.matches)) {
+        setSidebarOpen(false);
+      }
+    };
+
+    syncSidebar();
+
+    if (typeof mediaQuery.addEventListener === "function") {
+      mediaQuery.addEventListener("change", syncSidebar);
+
+      return () => mediaQuery.removeEventListener("change", syncSidebar);
+    }
+
+    mediaQuery.addListener(syncSidebar);
+
+    return () => mediaQuery.removeListener(syncSidebar);
+  }, []);
+
   return (
     <SidebarContext.Provider value={{ isOpen: sidebarOpen, toggle: () => setSidebarOpen(!sidebarOpen) }}>
-      <div className="flex min-h-screen w-full flex-col">
+      <div className="flex min-h-screen w-full flex-col lg:flex-row">
         <LeftMenu isOpen={sidebarOpen} products={mockProducts} />
         <Content>
           {children}
